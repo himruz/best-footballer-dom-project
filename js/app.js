0a416752cc2d777ca651ba3b692a8471d67380ca
Imruz360 <imruz2001@gmail.com>
2022-08-24 22:22:41 +0600

    update

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ const playerArray = [];
 
 function addPlayer(arr) {
 
-    console.log(arr)
     const selectedPlayerArea = document.getElementById('selected-players-id')
     selectedPlayerArea.innerHTML = '';
 
@@ -13,6 +12,10 @@ function addPlayer(arr) {
         const createPlayer = document.createElement('li');
         createPlayer.innerText = playerName;
         selectedPlayerArea.appendChild(createPlayer);
+    }
+    if (document.getElementById('selected-players-id').children.length > 5) {
+        alert('done')
+
     }
 }
 
@@ -24,6 +27,33 @@ function selectPlayer(element) {
     playerArray.push(selectedPlayer);
     addPlayer(playerArray)
 }
+
+
+document.getElementById('per-player-calculate').addEventListener('click', function () {
+    const getPerPlayerinput = document.getElementById('per-player')
+    const perPlayerString = getPerPlayerinput.value;
+    const perPlayerNumber = parseFloat(perPlayerString);
+    const playerExpenxe = perPlayerNumber * playerArray.length;
+    const perPlayerExpenxeField = document.getElementById('player-expenxe');
+    perPlayerExpenxeField.innerText = playerExpenxe;
+
+})
+
+document.getElementById('calculate-total').addEventListener('click', function () {
+    const getManagerinput = document.getElementById('manager')
+    const getManagerString = getManagerinput.value;
+    const getManagerNumber = parseFloat(getManagerString);
+
+    const getCoachinput = document.getElementById('coach')
+    const getCoachString = getCoachinput.value;
+    const getCoachNumber = parseFloat(getCoachString);
+    const perPlayerExpenxeField = document.getElementById('player-expenxe');
+    const totalPlayerExpenxe = perPlayerExpenxeField.innerText;
+    const totalPlayerExpenxeNumber = parseFloat(totalPlayerExpenxe)
+    const totalExpenxe = getManagerNumber + getCoachNumber + totalPlayerExpenxeNumber;
+    const getTotalExpexeField = document.getElementById('total')
+    getTotalExpexeField.innerText = totalExpenxe;
+})
 
 
 
